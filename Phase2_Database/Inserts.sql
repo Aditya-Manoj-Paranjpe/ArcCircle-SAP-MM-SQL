655ce938af2928082bb1414b8200678b4e6a26ab
Aditya-Manoj-Paranjpe <aditya.paranjpe8@gmail.com>
2026-01-27 12:05:18 +0530

    Inserts.sql

--- Phase2_Database/Inserts.sql
+++ Phase2_Database/Inserts.sql
@@ -1,14 +1,10 @@
 insert into Products
 (product_id, product_name, category, unit_price, plant_id, created_date, material_type)
-values
-(101,'Basmati Rice 5kg','Grocery',24.50,'ARC_PL01',NOW(),'RAW'),
-(102,'Wheat Flour 10kg','Grocery',18.75,'ARC_PL01',NOW(),'RAW'),
-(103,'LED Bulb 9W','Electronics',3.20,'ARC_PL02',NOW(),'FINISHED'),
-(104,'Custom Snack Box','Grocery',15.00,'ARC_PL01',NOW(),'SEMI_FINISHED');
--- Convert retail items to FINISHED goods
-update Products
-set material_type = 'FINISHED'
-where product_id in (101,102);
+Values
+(101,'Basmati Rice 5kg','Grocery',24.50,'ARC_PL01',NOW(),'Finished'),
+(102,'Wheat Flour 10kg','Grocery',18.75,'ARC_PL01',NOW(),'Finished'),
+(103,'LED Bulb 9W','Electronics',3.20,'ARC_PL02',NOW(),'Finished'),
+(104,'Custom Snack Box','Grocery',15.00,'ARC_PL01',NOW(),'Semi-finished');
 
 
 insert into Vendors
@@ -29,15 +25,16 @@ VALUES
 insert into Purchase_Orders
 (po_id, product_id, vendor_id, plant_id, quantity, order_date, delivery_days, status)
 values
-(301,101,201,'ARC_PL01',200,'2024-03-28',7,'Received'),
-(302,103,202,'ARC_PL02',500,'2024-04-05',14,'Received');
-
+(301,101,201,'ARC_PL01',200,DATE_SUB(CURDATE(),interval 10 day),7,'Received'),
+(302,102,201,'ARC_PL02',500,DATE_SUB(CURDATE(),interval 10 day),14,'Received'),
+(303,103,202,'ARC_PL01',300,DATE_SUB(CURDATE(),interval 10 day),7,'Received');
 
 
 insert into Sales_Orders
 (so_id, product_id, plant_id, storage_location, quantity, sale_date, region)
 values
-(401,101,'ARC_PL01','SL_001',50,'2026-01-10','North'),
-(402,102,'ARC_PL01','SL_001',30,'2026-01-11','West'),
-(403,103,'ARC_PL02','SL_002',70,'2026-01-12','East'),
-(404,104,'ARC_PL01','SL_001',20,'2026-01-13','South');
+(401,101,'ARC_PL01','SL_001',50,DATE_ADD(CURDATE(),interval 3 DAY),'North'),
+(402,102,'ARC_PL01','SL_001',30, DATE_ADD(CURDATE(),interval 4 DAY),'West'),
+(403,103,'ARC_PL02','SL_002',70, DATE_ADD(CURDATE(),interval 5 DAY),'East'),
+(404,104,'ARC_PL01','SL_001',20, DATE_ADD(CURDATE(),interval 6 DAY),'South');
+
