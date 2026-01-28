f01ac7e62f417b700bca7f6b1acaf4dfc8756bb6
Aditya-Manoj-Paranjpe <aditya.paranjpe8@gmail.com>
2026-01-28 10:48:52 +0530

    Business_Reports.sql

--- Phase3_Analytics/Business_Reports.sql
+++ Phase3_Analytics/Business_Reports.sql
@@ -1,4 +1,4 @@
--- Report 1- Vendor Performance analysis(Procurement Intelligence)
+-- Report 1: Vendor Performance analysis(Procurement Intelligence)
 -- Business Question(Which vendors are reliable and how efficiently are they supplying materials?)
 Select
 v.vendor_id,
@@ -34,34 +34,10 @@ order by f.fiscal_year, total_quantity_sold DESC;
 
 
 
--- Report 3: Slow Moving/ Dead Stock Detection(Inventory optimization)
--- Question: Which products are sitting in inventory without recent sales?
--- Logic: Find last sale date per product, Compare against today, Flag risk
-select
-p.product_id,
-p.product_name,
-i.storage_location,
-i.quantity,
-MAX(s.sale_date) AS last_sale_date,
-DATEDIFF(CURDATE(), MAX(s.sale_date)) AS days_since_last_sale
-from inventory i
-join products p
-on i.product_id = p.product_id
-left join sales_orders s
-on i.product_id = s.product_id
-group by
-p.product_id,
-p.product_name,
-i.storage_location,
-i.quantity
-having days_since_last_sale > 30
-or last_sale_date is null;
--- What this shows:Slow-moving inventory
--- “Slow-moving stock is detected purely through transactional analysis.
 
 
 
--- Report 4: Stock Consumption Trend(Window Function)
+-- Report 3: Stock Consumption Trend(Window Function)
 Select
 product_id,
 sale_date,
@@ -78,7 +54,7 @@ order by product_id, sale_date;
 
 
 
--- Report 5 - Region wise sales Contribution
+-- Report 4: Region wise sales Contribution
 select region,
 SUM(quantity) as total_units_sold
 from sales_orders
