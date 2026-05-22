f27c10888da5ee79dee4bed2a1aa893d5b09067e
Aditya <aditya.paranjpe8@gmail.com>
2026-05-22 22:12:29 +0530

    Inserts.sql

--- Phase2_Database/Inserts.sql
+++ Phase2_Database/Inserts.sql
@@ -1,41 +1,41 @@
-insert into Products
-(product_id, product_name, category, unit_price, plant_id, created_date, material_type)
-Values
-(101,'Basmati Rice 5kg','Grocery',24.50,'ARC_PL01',NOW(),'Finished'),
-(102,'Wheat Flour 10kg','Grocery',18.75,'ARC_PL01',NOW(),'Finished'),
-(103,'LED Bulb 9W','Electronics',3.20,'ARC_PL02',NOW(),'Finished'),
-(104,'Custom Snack Box','Grocery',15.00,'ARC_PL01',NOW(),'Semi-finished');
+INSERT INTO Products (product_name, category, unit_price, plant_id, material_type)
+VALUES 
+('Basmati Rice 5kg','Grocery',24.50,'ARC_PL01','Finished'),2
+('Wheat Flour 10kg','Grocery',18.75,'ARC_PL01','Finished'),
+('LED Bulb 9W','Electronics',3.20,'ARC_PL02','Finished'),
+('Custom Snack Box','Grocery',15.00,'ARC_PL01','Semi-finished');
+SELECT * FROM Products;
 
 
-insert into Vendors
-(vendor_id, vendor_name, country, purchasing_org, lead_time_days)
-values
-(201,'AgroFoods Ltd','India','ARC_PO01',7),
-(202,'Bright Electronics','China','ARC_PO01',15);
-
-
-insert into Inventory
-(product_id, storage_location, quantity, last_updated)
+INSERT INTO Vendors (vendor_name, country, purchasing_org, lead_time_days)
 VALUES
-(101,'SL_001',500,NOW()),
-(102,'SL_001',300,NOW()),
-(103,'SL_002',800,NOW()),
-(104,'SL_001',100,NOW());
+('AgroFoods Ltd','India','ARC_PO01',7),
+('Bright Electronics','China','ARC_PO01',15);
+SELECT * FROM Vendors;
 
 
-insert into Purchase_Orders
-(po_id, product_id, vendor_id, plant_id, quantity, order_date, delivery_days, status)
-values
-(301,101,201,'ARC_PL01',200,DATE_SUB(CURDATE(),interval 10 day),7,'Received'),
-(302,102,201,'ARC_PL01',500,DATE_SUB(CURDATE(),interval 10 day),14,'Received'),
-(303,103,202,'ARC_PL01',300,DATE_SUB(CURDATE(),interval 10 day),7,'Received');
+INSERT INTO Inventory (product_id, storage_location, quantity, last_updated)
+VALUES
+(1,'SL_001',500,NOW()),
+(2,'SL_001',300,NOW()),
+(3,'SL_002',800,NOW()),
+(4,'SL_001',100,NOW());
+SELECT * FROM Inventory;
 
 
-insert into Sales_Orders
-(so_id, product_id, plant_id, storage_location, quantity, sale_date, region)
-values
-(401,101,'ARC_PL01','SL_001',50,DATE_ADD(CURDATE(),interval 3 DAY),'North'),
-(402,102,'ARC_PL01','SL_001',30, DATE_ADD(CURDATE(),interval 4 DAY),'West'),
-(403,103,'ARC_PL02','SL_002',70, DATE_ADD(CURDATE(),interval 5 DAY),'East'),
-(404,104,'ARC_PL01','SL_001',20, DATE_ADD(CURDATE(),interval 6 DAY),'South');
+INSERT INTO Purchase_Orders (product_id, vendor_id, plant_id, quantity, order_date, delivery_days, status)
+VALUES
+(1,1,'ARC_PL01',200,DATE_SUB(CURDATE(), INTERVAL 10 DAY),7,'Received'),
+(2,1,'ARC_PL01',500,DATE_SUB(CURDATE(), INTERVAL 10 DAY),14,'Received'),
+(3,2,'ARC_PL02',300,DATE_SUB(CURDATE(), INTERVAL 10 DAY),7,'Received');
+SELECT * FROM Purchase_Orders;
+
+
+INSERT INTO Sales_Orders (product_id, plant_id, storage_location, quantity, sale_date, region)
+VALUES
+(1,'ARC_PL01','SL_001',50,DATE_ADD(CURDATE(), INTERVAL 3 DAY),'North'),
+(2,'ARC_PL01','SL_001',30,DATE_ADD(CURDATE(), INTERVAL 4 DAY),'West'),
+(3,'ARC_PL02','SL_002',70,DATE_ADD(CURDATE(), INTERVAL 5 DAY),'East'),
+(4,'ARC_PL01','SL_001',20,DATE_ADD(CURDATE(), INTERVAL 6 DAY),'South');
+SELECT * FROM Sales_Orders;
 
